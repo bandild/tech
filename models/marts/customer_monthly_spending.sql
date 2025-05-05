354884011c3e9ba120a70f36585496079f3e1c37
Lakshma Reddy Bandi <lakshmareddy1266@gmail.com>
2025-05-05 02:50:53 +0000

    commit

--- models/marts/customer_monthly_spending.sql
+++ models/marts/customer_monthly_spending.sql
@@ -33,27 +33,3 @@ GROUP BY cu.customer_id, cu.first_name, cu.age, cu.account_status,
 
 
 
-models
-/
-marts
-/
-customer_monthly_spending.sql
-
-Save
-910118121314151617181920212223
-  COUNT(tx.transaction_id) AS txn_count,
-  SUM(tx.amount) AS total_spent,
-  AVG(tx.amount) AS avg_txn_value,
-  MAX(tx.amount) AS max_txn
-
-FROM {{ ref('fact_transactions') \}\} tx
-LEFT JOIN {{ ref('dim_customers') \}\} cu ON tx.customer_id = cu.customer_id 
-LEFT JOIN {{ ref('dim_accounts') \}\} ac ON tx.customer_id = ac.customer_id  
-
-WHERE tx.transaction_status = 'COMPLETED' AND cu.customer_id IS NOT NULL
-
-3SELECT
-
-Enter dbt command
-dbt build --select <model_name>
-$0
